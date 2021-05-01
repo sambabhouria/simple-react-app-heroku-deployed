a635802bab8d1f4db5abb5f7d1b8a5d96b3d6f5f
Samba Diallo <samba.diallo@id-mac007.idtgv.cnit>
2021-05-01 21:55:55 +0200

    updating package

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const path = require("path")
 
+const port = process.env.PORT || 8080;
+
 const app = express()
 
 app.use(express.static(path.join(__dirname, "build")))
@@ -11,3 +13,5 @@ app.get("/", function (req, res) {
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
+
+app.listen(port);
